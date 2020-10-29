0db3c2005aff4f2ff3c07185d8bb4d21aad43049
ThaSMorato <tha.s.morato@gmail.com>
2020-10-28 23:53:28 -0300

    Locale in interface

--- wizebot-web/src/templates/PlayerInterface.ts
+++ wizebot-web/src/templates/PlayerInterface.ts
@@ -2,10 +2,11 @@ import React from 'react';
 import PlayerEloInterface from './PlayerEloInterface';
 
 interface PlayerInterface{
+    locale: string;
+    name: string;
     id: string;
     accountId: string;
     puuid: string;
-    name: string;
     profileIconId: number;
     revisionDate: number;
     summonerLevel: number;
@@ -13,3 +14,20 @@ interface PlayerInterface{
 }
 
 export default PlayerInterface;
+
+
+/**
+ *       
+            <option value="BR1" selected="true">BR1</option>
+            <option value="EUN1">EUN1</option>
+            <option value="EUW1">EUW1</option>
+            <option value="JP1">JP1</option>
+            <option value="KR">KR</option>
+            <option value="LA1">LA1</option>
+            <option value="LA2">LA2</option>
+            <option value="NA1">NA1</option>
+            <option value="OC1">OC1</option>
+            <option value="RU">RU</option>
+            <option value="TR1">TR1</option>
+                
+ */
